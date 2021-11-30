39ab3294ebe668a06d0485bddd704a6dbfaecbc5
sm3131 <sm2683@nau.edu>
2021-11-30 01:58:19 -0700

    code clean up

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,22 @@ var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var formSubmitHandler = function (event) {
+    // prevent page from refreshing
+    event.preventDefault();
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+
+        //clear old content
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
 
 var getUserRepos = function (user) {
     // format the github api url
@@ -24,29 +40,13 @@ var getUserRepos = function (user) {
       });
 };
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    //get value from input element
-    var username = nameInputEl.value.trim();
-
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
-        alert("Please enter a GitHub username");
-    }
-
-    console.log(event);
-};
-
 var displayRepos = function (repos, searchTerm) {
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     }
-    console.log(repos);
-    console.log(searchTerm);
+    
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
